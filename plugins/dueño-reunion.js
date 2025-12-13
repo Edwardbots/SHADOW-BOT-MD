df0cf80d77b667d934deb6e6977e6e5536be071c
yosue891 <yosueortega630@gmail.com>
2025-12-13 08:10:54 -0400

    Update dueño-reunion.js

--- plugins/dueño-reunion.js
+++ plugins/dueño-reunion.js
@@ -1,12 +1,15 @@
-let handler = async(m, { conn, command, text }) => {
+let handler = async (m, { conn, command, text }) => {
+  const emoji = '📢'
+  const emoji2 = '⚠️'
+
   if (!text) return m.reply(`${emoji} Por favor, ingresa el motivo de la reunión.`)
   if (text.length < 10) return m.reply(`${emoji2} Por favor, ingresa al menos 10 caracteres.`)
-  
+
   let texto = `${emoji2} El Owner @${m.sender.split`@`[0]} ha empezado una reunión. Entra lo más pronto al grupo de staff...\n*➪ Motivo: ${text}*`
   m.reply(`${emoji} Enviando mensaje de reunión a todos los owners.`)
-  
+
   let mentions = [m.sender]
-  
+
   for (let [jid] of global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
     let data = (await conn.onWhatsApp(jid))[0] || {}
     if (data.exists) {
